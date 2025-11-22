268212da143824c0665f6b124537f36f5ab0f980
Bảo Nguyên <118587919+baonguyen132@users.noreply.github.com>
2025-11-22 08:46:24 +0700

    upload

--- lib/data/ConstraintData.dart
+++ lib/data/ConstraintData.dart
@@ -5,7 +5,7 @@ var mainLightDarkMode = true ;
 
 const location = "https://steady-sharply-ibex.ngrok-free.app" ;
 // const location = "http://localhost:5000" ;
-const apiAI = "https://b0313c7f2b38.ngrok-free.app/webhook/bookswap" ;
+const apiAI = "https://2d7320c68082.ngrok-free.app/webhook/bookswap" ;
 
 // Hàm chuyển số CCCD thành ngày tháng năm
 String formatIDToDate(String idNumber) {
---
+++ lib/screens/dashboard/page/manager/cubit/post_facebook/post_facebook_cubit.dart
@@ -0,0 +1,14 @@
+//
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:freezed_annotation/freezed_annotation.dart';
+//
+// import '../../../../../../model/user_modal.dart';
+//
+// part 'manage_user_state.dart';
+// part 'manage_user_cubit.freezed.dart';
+//
+// class PostFacebookCubit extends Cubit<PostFacebookState> {
+//   ManageUserCubit() : super(const ManageUserState.initial());
+//
+//
+// }
