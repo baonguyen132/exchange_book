36e8bcce548216c43f76626191da0f60b7f4b519
Bảo Nguyên <118587919+baonguyen132@users.noreply.github.com>
2026-04-04 19:58:58 +0700

    upload

--- lib/data/ConstraintData.dart
+++ lib/data/ConstraintData.dart
@@ -5,7 +5,7 @@ var mainLightDarkMode = true ;
 
 const location = "https://steady-sharply-ibex.ngrok-free.app" ;
 // const location = "http://localhost:5000" ;
-const apiAI = "https://13517536ad15.ngrok-free.app/webhook/bookswap" ;
+const apiAI = "https://f4d0-2402-800-6e27-fb34-2dc4-f66f-8452-53bd.ngrok-free.app/webhook/bookswap" ;
 
 // Hàm chuyển số CCCD thành ngày tháng năm
 String formatIDToDate(String idNumber) {
@@ -27,4 +27,4 @@ void toast(String content) {
     textColor: Colors.white,
     fontSize: 16.0,
   );
-}
+} 
